d94707b93bb615b5a768a47da2fa22d3299c93af
ovez khan <khanovez257@gmail.com>
2025-03-11 12:59:32 +0530

    Fix: React import issue and updated Vite config

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,5 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
+import React from 'react';
+
 
 
 export const StoreContext = createContext(null);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,46 @@
 //   plugins: [react()],
 // })
 
+
+
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   base: './',
+//   build: {
+//     outDir: 'dist',
+//   },
+// });
+
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: './',
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',
+//   },
+// });
+
+
+
+
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  plugins: [react()],
   build: {
     outDir: 'dist',
   },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 });
+
