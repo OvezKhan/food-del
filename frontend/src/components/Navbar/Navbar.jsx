ba3eb9d2f65de80c50a9e2218f234a2cc5986aa4
OvezKhan <161130711+OvezKhan@users.noreply.github.com>
2025-03-25 17:57:21 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link, useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
+import footBiteLogo from '../assets/foot_bite_logo.png';
+
 
 
 const Navbar = ({setShowLogin }) => {
